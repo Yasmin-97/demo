b9e3292d4c2adfe2e3ec8b0cfac760921a9f2518
Trysol Global Services <127195129+trysol2023@users.noreply.github.com>
2023-03-09 12:37:23 +0530

    newproject

---
+++ src/main/java/com/example/Controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.example.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Model.Employee;
+import com.example.Repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+@Autowired
+private EmployeeRepository employeeRepository;
+@GetMapping("/employees")
+public List<Employee> getAllEmployees(){
+	return employeeRepository.findAll();
+}
+}
---
+++ src/main/java/com/example/Model/Employee.java
@@ -0,0 +1,66 @@
+package com.example.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(name = "first_name")
+	private String firstname;
+	@Column(name = "last_name")
+	private String lastname;
+	@Column(name = "emailid_name")
+	private String emailid;
+	
+	public Employee() {
+
+	}
+
+	public Employee(String firstname, String lastname, String emailid) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.emailid = emailid;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmailid() {
+		return emailid;
+	}
+
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+}
